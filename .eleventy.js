09f786d456e13fc1ed66ed1bdc28a5fbb07b318a
Glenn Goossens <glgoose@pm.me>
2021-09-08 10:20:31 +0200

    restore

---
+++ .eleventy.js
@@ -0,0 +1,77 @@
+const pluginRss = require('@11ty/eleventy-plugin-rss')
+const markdownIt = require('markdown-it')
+const yaml = require("js-yaml");
+
+const filters = require('./utils/filters.js')
+const transforms = require('./utils/transforms.js')
+const shortcodes = require('./utils/shortcodes.js')
+const iconsprite = require('./utils/iconsprite.js')
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = function (config) {
+    // Plugins
+    config.addPlugin(pluginRss)
+
+    // Filters
+    Object.keys(filters).forEach((filterName) => {
+        config.addFilter(filterName, filters[filterName])
+    })
+
+    // Transforms
+    Object.keys(transforms).forEach((transformName) => {
+        config.addTransform(transformName, transforms[transformName])
+    })
+
+    // Shortcodes
+    Object.keys(shortcodes).forEach((shortcodeName) => {
+        config.addShortcode(shortcodeName, shortcodes[shortcodeName])
+    })
+
+    // Icon Sprite
+    config.addNunjucksAsyncShortcode('iconsprite', iconsprite)
+
+    // Asset Watch Targets
+    config.addWatchTarget('./src/assets')
+
+    // Markdown
+    config.setLibrary(
+        'md',
+        markdownIt({
+            html: true,
+            breaks: true,
+            linkify: true,
+            typographer: true
+        })
+    )
+
+    // YAML
+    config.addDataExtension("yaml", contents => yaml.safeLoad(contents));
+
+    // Layouts
+    config.addLayoutAlias('base', 'cv/layouts/base.njk')
+    config.addLayoutAlias('cv', 'cv/layouts/cv.njk')
+
+    // Pass-through files
+    config.addPassthroughCopy('src/robots.txt')
+    config.addPassthroughCopy('src/assets/images')
+    config.addPassthroughCopy('src/assets/fonts')
+    config.addPassthroughCopy('src/_redirects')
+
+    // Deep-Merge
+    config.setDataDeepMerge(true)
+
+    // Base Config
+    return {
+        dir: {
+            input: 'src',
+            output: 'dist',
+            includes: '_includes',
+            data: '_data'
+        },
+        templateFormats: ['njk', 'md', '11ty.js'],
+        htmlTemplateEngine: 'njk',
+        markdownTemplateEngine: 'njk'
+    }
+}
